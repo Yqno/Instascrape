f7881515fcb574af8fc0d9b0733399d3e3cc0ef6
Y Q N O <79216939+Yqno@users.noreply.github.com>
2022-11-29 07:30:32 +0100

    Create instascrape.py

---
+++ instascrape.py
@@ -0,0 +1,16 @@
+ # Import the module
+import instaloader
+
+# Create an instance of Instaloader class
+bot = instaloader.Instaloader()
+
+# Load a profile from an Instagram handle
+profile = instaloader.Profile.from_username(bot.context, 'usernamehere')
+
+print(type(profile))
+print("Username: ", profile.username)
+print("User ID: ", profile.userid)
+print("Number of Posts: ", profile.mediacount)
+print("Followers: ", profile.followers)
+print( "Followees: ", profile.followees)
+print( "Bio: ", profile.biography,profile.external_url)
